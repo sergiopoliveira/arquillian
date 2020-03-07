6f11b193777d0344df9494084b2223e3efef371e
sergiopoliveira <sergiopoliveira@outlook.com>
2020-03-07 19:16:16 +0000

    Arquillian test

---
+++ src/test/java/components/GreeterTest.java
@@ -0,0 +1,34 @@
+package components;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(Arquillian.class)
+public class GreeterTest {
+
+    @Inject
+    Greeter greeter;
+
+    @Test
+    public void should_create_greeting() {
+        assertEquals("Hello, Earthling!", greeter.createGreeting("Earthling"));
+        greeter.greet(System.out, "Earthling");
+    }
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(Greeter.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+}
