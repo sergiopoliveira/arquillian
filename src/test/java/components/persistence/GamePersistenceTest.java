864a0eb9ab348965af04e88e0c9aa2c9f9fec402
sergiopoliveira <sergiopoliveira@outlook.com>
2020-03-08 15:55:03 +0000

    Query with JPQL

--- src/test/java/components/persistence/GamePersistenceTest.java
+++ src/test/java/components/persistence/GamePersistenceTest.java
@@ -7,13 +7,18 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
+import java.util.*;
+
+import static org.junit.Assert.*;
 
 @RunWith(Arquillian.class)
 public class GamePersistenceTest {
@@ -78,5 +83,29 @@ public class GamePersistenceTest {
     }
 
 // tests go here
+
+    @Test
+    public void shouldFindAllGamesUsingJpqlQuery() throws Exception {
+        // given
+        String fetchingAllGamesInJpql = "select g from Game g order by g.id";
+
+        // when
+        System.out.println("Selecting (using JPQL)...");
+        List<Game> games = em.createQuery(fetchingAllGamesInJpql, Game.class).getResultList();
+
+        // then
+        System.out.println("Found " + games.size() + " games (using JPQL):");
+        assertContainsAllGames(games);
+    }
+
+    private static void assertContainsAllGames(Collection<Game> retrievedGames) {
+        assertEquals(GAME_TITLES.length, retrievedGames.size());
+        final Set<String> retrievedGameTitles = new HashSet<String>();
+        for (Game game : retrievedGames) {
+            System.out.println("* " + game);
+            retrievedGameTitles.add(game.getTitle());
+        }
+        assertTrue(retrievedGameTitles.containsAll(Arrays.asList(GAME_TITLES)));
+    }
 
 }
