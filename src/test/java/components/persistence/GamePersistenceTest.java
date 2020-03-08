b558d3cd3fecafb1992f0aac8928d827754e808f
sergiopoliveira <sergiopoliveira@outlook.com>
2020-03-08 15:51:06 +0000

    Write the Test

---
+++ src/test/java/components/persistence/GamePersistenceTest.java
@@ -0,0 +1,82 @@
+package components.persistence;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+
+@RunWith(Arquillian.class)
+public class GamePersistenceTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Inject
+    UserTransaction utx;
+
+    private static final String[] GAME_TITLES = {
+            "Super Mario Brothers",
+            "Mario Kart",
+            "F-Zero"
+    };
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+                .addPackage(Game.class.getPackage())
+                .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+
+    @Before
+    public void preparePersistenceTest() throws Exception {
+        clearData();
+        insertData();
+        startTransaction();
+    }
+
+    private void clearData() throws Exception {
+        utx.begin();
+        em.joinTransaction();
+        System.out.println("Dumping old records...");
+        em.createQuery("delete from Game").executeUpdate();
+        utx.commit();
+    }
+
+    private void insertData() throws Exception {
+        utx.begin();
+        em.joinTransaction();
+        System.out.println("Inserting records...");
+        for (String title : GAME_TITLES) {
+            Game game = new Game(title);
+            em.persist(game);
+        }
+        utx.commit();
+        // clear the persistence context (first-level cache)
+        em.clear();
+    }
+
+    private void startTransaction() throws Exception {
+        utx.begin();
+        em.joinTransaction();
+    }
+
+    @After
+    public void commitTransaction() throws Exception {
+        utx.commit();
+    }
+
+// tests go here
+
+}
